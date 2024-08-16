86ebdebcfdf63d8186a462d034dd35091098953b
-2 <>
2024-08-16 11:07:58 -0300

    terminado

--- basedatos_salon.sql
+++ basedatos_salon.sql
@@ -28,4 +28,14 @@ add column email VARCHAR(50) NOT NULL;
 ALTER TABLE cliente
 add column telefono int default 0;
 
+create TABLE evento (
+    nro_evento int AUTO_INCREMENT,
+    fecha VARCHAR(10) NOT NULL,
+    cliente_id int,
+    primary key(nro_evento),
+    foreign key (cliente_id) references cliente(id_cliente)
+);
 
+
+INSERT into evento (fecha_temp,) VALUES (2024-04-12:20:00:00,);
+UPDATE evento Set fecha_temp =(NOW());
